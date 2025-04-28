26e0bc8fb7b1214ec1e9f85d7af6a655f5b1b71d
adeleltaher <adel.eltaher@student.supsi.ch>
2025-04-28 13:43:05 +0200

    added file to recreate db on other devs machine

---
+++ backend/database/brughera_db.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.13  Distrib 9.2.0, for macos15.2 (arm64)
+--
+-- Host: localhost    Database: inventory_db
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `longitude` double NOT NULL,
+  `latitude` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Trattoria del Lago',8.951,46.0059),(2,'Ristorante Bella Vita',8.9605,46.0078),(3,'Osteria Ticinese',8.9487,46.0032),(4,'Pizzeria Il Sorriso',8.9612,46.0101),(5,'Locanda Alpina',8.9555,46.012);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_prices`
+--
+
+DROP TABLE IF EXISTS `item_prices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_prices` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_prices`
+--
+
+LOCK TABLES `item_prices` WRITE;
+/*!40000 ALTER TABLE `item_prices` DISABLE KEYS */;
+INSERT INTO `item_prices` VALUES (1,'Perrier',15.00),(2,'San Clemente',17.00),(3,'Valser',19.00),(4,'Beer keg',10.00),(5,'Chopfab Doppelleu',20.00),(6,'Epti',20.00),(7,'Feldschlösschen Bier',20.00),(8,'Gazzose',16.00),(9,'Hacker-Pschorr',15.00),(10,'Henniez',21.00),(11,'Appenzeller Bier',14.00),(12,'Pomd\'or Suisse',14.00),(13,'Michel',17.00),(14,'Coca-Cola',14.00),(15,'Unknown red crate',19.00),(16,'Drinks',11.00),(17,'Rivella',12.00),(18,'Water bottle',10.00),(19,'Schweppes',13.00),(20,'Acqua Panna',15.00);
+/*!40000 ALTER TABLE `item_prices` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-28 13:38:24
